fc81335f78023e93ca53f3d4dbf4ccb90b7f2d40
Sushil <Sushil@Sushil-PC>
2021-06-19 17:22:53 +0530

    PsBlobInsertTest

--- JDBCProj1/src/com/nit/jdbc1/PsBlobInsertMySQLTest.java
+++ JDBCProj1/src/com/nit/jdbc1/PsBlobInsertMySQLTest.java
@@ -8,17 +8,17 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.Scanner;
 
-/*CREATE TABLE "SYSTEM"."ARTIST_INFO" 
-(	"AID" NUMBER(10,0), 
-	"NAME" VARCHAR2(20 BYTE), 
-	"ADDRS" VARCHAR2(20 BYTE), 
-	"PHOTO" BLOB
-)
-CREATE SEQUENCE  "SYSTEM"."AID_SEQ"  MINVALUE 1000 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 1000 CACHE 20 NOORDER  NOCYCLE ;
- */
+/*CREATE TABLE `artist_info` (
+		  `aid` int NOT NULL AUTO_INCREMENT,
+		  `name` varchar(15) DEFAULT NULL,
+		  `addrs` varchar(15) DEFAULT NULL,
+		  `photo` longblob,
+		  PRIMARY KEY (`aid`),
+		  UNIQUE KEY `aid_UNIQUE` (`aid`)
+		) */
 
-public class PsBlobInsertTest {
-	private static final String INSERT_ARTIST_QUERY = "INSERT INTO ARTIST_INFO VALUES(AID_SEQ.NEXTVAL,?,?,?)";
+public class PsBlobInsertMySQLTest {
+	private static final String INSERT_ARTIST_QUERY = "INSERT INTO ARTIST_INFO(NAME,ADDRS,PHOTO) VALUES(?,?,?)";
 
 	public static void main(String[] args) {
 		try(Scanner sc = new Scanner(System.in)){
@@ -29,13 +29,13 @@ public class PsBlobInsertTest {
 				name = sc.next();
 				System.out.println("Enter artist address: ");
 				addrs = sc.next();
-				System.out.println("Enter artist location: ");
-				photoLocation = sc.next();
+				System.out.println("Enter artist photo location: ");
+				photoLocation = sc.next().replace("?","");
 			}//if
 			//Create inputStream pointing to photo file
 			try(InputStream is = new FileInputStream(photoLocation)){
 				//Establish the connection and prepare statement object
-				try(Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","system","manager");
+				try(Connection con = DriverManager.getConnection("jdbc:mysql:///ntaj415db2","root","root");
 						PreparedStatement ps = con.prepareStatement(INSERT_ARTIST_QUERY); ){
 					//Set values to query param
 					if(ps!=null) {
