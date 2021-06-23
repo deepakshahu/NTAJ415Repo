fdfe23a74d1c110825f80066a3b58a7a39dbb6fa
Sushil <Sushil@Sushil-PC>
2021-06-23 23:03:54 +0530

    PostgreSQL

--- JDBCProj1/src/com/nit/jdbc1/PsAgeCalculatorPostgreSQL.java
+++ JDBCProj1/src/com/nit/jdbc1/PsAgeCalculatorPostgreSQL.java
@@ -11,86 +11,41 @@ import java.util.Scanner;
 public class PsAgeCalculatorPostgreSQL {
 	private static final String AGE_CALCULATOR="SELECT AGE(dob) FROM PERSON WHERE PID=?";
 	public static void main(String[] args) {
-		Scanner sc = null;
-		Connection con = null;
-		PreparedStatement ps = null;
-		ResultSet rs = null;
-		try {
-			//Read inputs
-			sc = new Scanner(System.in);
+		try(Scanner sc = new Scanner(System.in) ){
 			int pid = 0;
 			if(sc!=null) {
 				System.out.println("Enter Person id: ");
 				pid = sc.nextInt();
 			}
+			try(//Establish the Connection with Oracle DB s/w
+					Connection con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/NTAJ415DB", "postgres", "tiger");
+					//Create Statement object
+					PreparedStatement ps = con.prepareStatement(AGE_CALCULATOR);	  ){
 
-			//Load JDBC Driver class
-			Class.forName("org.postgresql.Driver");
+				//set value to query parameters
+				if(ps!=null)
+					ps.setInt(1,pid);
 
-			//Establish the connection
-			//con = DriverManager.getConnection("jdbc:postgresql:NTAJ415DB","postgres","tiger");
-			con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/NTAJ415DB", "postgres", "tiger");
-
-			//crate JDBC PreparedStatement object having pre-complied sql query
-			if(con!=null)
-				ps = con.prepareStatement(AGE_CALCULATOR);
-
-			//set value to query parameters
-			if(ps!=null)
-				ps.setInt(1,pid);
-
-			//execute the query
-			if(ps!=null)
-				rs = ps.executeQuery();
-
-			//Process the ResultSet
-			if(rs!=null) {
-				if(rs.next()) {
-					String age = rs.getString(1);
-					System.out.println("Person age is: "+age);
-				}
-				else {
-					System.out.println("Person not found");
-				}
-
-			}//if
-		}//try
+				//Execute the query
+				try(ResultSet rs = ps.executeQuery()){
+					//Process the ResultSet
+					if(rs!=null) {
+						if(rs.next()) {
+							String age = rs.getString(1);
+							System.out.println("Person age is: "+age);
+						}
+						else {
+							System.out.println("Person not found");
+						}
+					}//if
+				}//try3
+			}//try2
+		}//try1
 		catch(SQLException se) {
 			se.printStackTrace();
 		}
 		catch(Exception e) {
 			e.printStackTrace();
 		}
-		finally {
-			//Close JDBC objects
-			try {
-				if(rs!=null)
-					rs.close();
-			}
-			catch(SQLException se) {
-				se.printStackTrace();
-			}
-			try {
-				if(ps!=null)
-					ps.close();
-			}
-			catch(SQLException se) {
-				se.printStackTrace();
-			}
-			try {
-				if(con!=null)
-					con.close();
-			}
-			catch(SQLException se) {
-				se.printStackTrace();
-			}
-			try {
-				if(sc!=null)
-					sc.close();
-			}
-			catch(Exception e) {
-				e.printStackTrace();
-			}
-		}//finally
 	}//main
 }//class
