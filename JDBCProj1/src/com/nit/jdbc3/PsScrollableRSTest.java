b24eb79f6ca409d56f1251addad4dfeb1076ad57
Sushil <Sushil@Sushil-PC>
2021-07-17 22:13:54 +0530

    PsScrollableRsTest

---
+++ JDBCProj1/src/com/nit/jdbc3/PsScrollableRSTest.java
@@ -0,0 +1,55 @@
+package com.nit.jdbc3;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class PsScrollableRSTest {
+	private static final String EMP_SELECT_QUERY="SELECT EMPNO,ENAME,JOB,SAL FROM EMP";
+
+	public static void main(String[] args) {
+		try(Connection con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "system", "manager");
+				PreparedStatement ps = con.prepareStatement(EMP_SELECT_QUERY,ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+				ResultSet rs = ps.executeQuery();  ){
+			if(rs!=null) {
+				System.out.println("RS records top to bottom");
+				while(rs.next()) {
+					System.out.println(rs.getRow()+"----->"+rs.getInt(1)+" "+rs.getString(2)+" "+rs.getString(3)+" "+rs.getString(4));
+				}
+				System.out.println("----------------------------------------------------");
+				System.out.println("RS records bottom to top");
+				rs.afterLast();
+				while(rs.previous()) {
+					System.out.println(rs.getRow()+"----->"+rs.getInt(1)+" "+rs.getString(2)+" "+rs.getString(3)+" "+rs.getString(4));
+				}
+				System.out.println("----------------------------------------------------");
+				rs.first();
+				System.out.println(rs.getRow()+"----->"+rs.getInt(1)+" "+rs.getString(2)+" "+rs.getString(3)+" "+rs.getString(4));
+				System.out.println("----------------------------------------------------");
+				rs.last();
+				System.out.println(rs.getRow()+"----->"+rs.getInt(1)+" "+rs.getString(2)+" "+rs.getString(3)+" "+rs.getString(4));
+				System.out.println("----------------------------------------------------");
+				rs.absolute(3);
+				System.out.println(rs.getRow()+"----->"+rs.getInt(1)+" "+rs.getString(2)+" "+rs.getString(3)+" "+rs.getString(4));
+				System.out.println("----------------------------------------------------");
+				rs.absolute(-6);
+				System.out.println(rs.getRow()+"----->"+rs.getInt(1)+" "+rs.getString(2)+" "+rs.getString(3)+" "+rs.getString(4));
+				System.out.println("----------------------------------------------------");
+				rs.relative(3);
+				System.out.println(rs.getRow()+"----->"+rs.getInt(1)+" "+rs.getString(2)+" "+rs.getString(3)+" "+rs.getString(4));
+				System.out.println("----------------------------------------------------");
+				rs.relative(-6);
+				System.out.println(rs.getRow()+"----->"+rs.getInt(1)+" "+rs.getString(2)+" "+rs.getString(3)+" "+rs.getString(4));
+				System.out.println("----------------------------------------------------");
+			}//if
+		}//try
+		catch(SQLException se) {
+			se.printStackTrace();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}//main
+}//class
